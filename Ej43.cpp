3be1a941a37ebcd3d470301921531d74d5658ebc
Fabian2112 <67841089+Fabian2112@users.noreply.github.com>
2022-06-01 20:47:26 -0300

    Add files via upload

---
+++ Ej43.cpp
@@ -0,0 +1,100 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+#include<iostream>
+using namespace std;
+
+// Para las variables que no se pudo determinar el tipo se utiliza la constante
+// SIN_TIPO. El usuario debe reemplazar sus ocurrencias por el tipo adecuado
+// (usualmente int,float,string o bool).
+#define SIN_TIPO string
+
+// Para leer variables de texto se utiliza el operador << del objeto cin, que
+// lee solo una palabra. Para leer una linea completa (es decir, incluyendo los
+// espacios en blanco) se debe utilzar getline (ej, reemplazar cin>>x por
+// getline(cin,x)), pero obliga a agregar un cin.ignore() si antes del getline
+// se leyó otra variable con >>.
+
+// Declaraciones adelantadas de las funciones
+bool entre25y40mil(float sueldo);
+
+int main() {
+	string bs;
+	float cempleados;
+	int csue25y40;
+	int csue30;
+	int dni;
+	int dnimaxsue;
+	float maxcempleados;
+	float maxcemppleados;
+	int maxcsue30;
+	float maxsue;
+	int n;
+	int ns;
+	int sede;
+	int sedemaxcempleados;
+	int sedemaxcsue30;
+	float sue;
+	SIN_TIPO suesde;
+	float suesede;
+	float suetot;
+	int tt;
+	n = 3;
+	maxcsue30 = 0;
+	suetot = 0;
+	maxsue = 0;
+	maxcemppleados = 0;
+	for (sede=1;sede<=n;sede++) {
+		suesede = 0;
+		csue25y40 = 0;
+		cempleados = 0;
+		cout << "Ingrese el Numero de la sede" << endl;
+		cin >> ns;
+		cout << "Ingrese el barrio de la sede Nro " << ns << endl;
+		cin >> bs;
+		cout << "Ingrese el DNI del empleado" << endl;
+		cin >> dni;
+		while (dni!=0) {
+			cout << "Ingrese Sueldo" << endl;
+			cin >> sue;
+			cout << "Ingrese tipo de tarea (1-10)" << endl;
+			cin >> tt;
+			if (entre25y40mil(sue)) {
+				csue25y40 = csue25y40+1;
+			}
+			suesede = suesede+sue;
+			cempleados = cempleados+1;
+			if (tt==1) {
+				if (sue>maxsue) {
+					maxsue = sue;
+					dnimaxsue = dni;
+				}
+			}
+			cout << "Ingrese el DNI del empleado" << endl;
+			cin >> dni;
+		}
+		if (cempleados>maxcempleados) {
+			maxcempleados = cempleados;
+			sedemaxcempleados = ns;
+		}
+		cout << "En sede " << bs << " hay " << csue25y40 << " empleados con sueldos entre $25000 y $40000" << endl;
+		cout << "En Sede " << bs << " hay que abonar $" << suesde << " de sueldos" << endl;
+		suetot = suetot+suesede;
+	}
+	cout << "Sede con > cant de empleados es " << sedemaxcempleados << " con " << cempleados << " empleados" << endl;
+	cout << "El empleados con mayor sueldo es DNI " << dnimaxsue << endl;
+	cout << "Entre todas las sedes hay que abonar $" << suetot << " de sueldos" << sue << endl;
+	return 0;
+}
+
+bool entre25y40mil(float sueldo) {
+	bool e20y40mil;
+	if (sueldo>=25000 && sueldo<=40000) {
+		e20y40mil = true;
+	} else {
+		e20y40mil = false;
+	}
+	return e20y40mil;
+}
+
---
+++ Ej43.psc
@@ -0,0 +1,62 @@
+Algoritmo Ej43 
+	Definir sede,N,NS,DNI,TT,CSue25y40,DNIMaxSue,CSue30,MaxCSue30,SedeMaxCSue30 como entero
+	Definir Sue,MaxSue,SueSede,SueTot como real
+	Definir BS como caracter
+	N <- 3
+	MaxCSue30 <- 0
+	SueTot <- 0
+	MaxSue <- 0
+	MaxCemppleados <- 0
+	Para sede<-1 Hasta N Hacer
+		SueSede <- 0
+		CSue25y40 <- 0
+		Cempleados <- 0
+		Escribir 'Ingrese el Numero de la sede'
+		Leer NS
+		Escribir 'Ingrese el barrio de la sede Nro ',NS
+		Leer BS
+		Escribir 'Ingrese el DNI del empleado'
+		Leer DNI
+		Mientras DNI<>0 Hacer
+			Escribir 'Ingrese Sueldo'
+			Leer Sue
+			Escribir 'Ingrese tipo de tarea (1-10)'
+			Leer TT
+			Si Entre25y40mil(Sue) Entonces
+				CSue25y40 <- CSue25y40+1
+			FinSi
+			SueSede <- SueSede+Sue
+			Cempleados <- Cempleados+1
+			Si TT=1 Entonces
+				Si Sue>MaxSue Entonces
+					MaxSue <- Sue
+					DNIMaxSue <- DNI
+				FinSi
+			FinSi
+			Escribir 'Ingrese el DNI del empleado'
+			Leer DNI
+		FinMientras
+		Si Cempleados>MaxCempleados Entonces
+			MaxCempleados <- Cempleados
+			SedeMaxCempleados <- NS
+		FinSi
+		Escribir 'En sede ',BS,' hay ',CSue25y40,' empleados con sueldos entre $25000 y $40000'
+		Escribir 'En Sede ',BS,' hay que abonar $',SueSde,' de sueldos'
+		SueTot <- SueTot+SueSede
+	FinPara
+	Escribir "Sede con > cant de empleados es ",SedeMaxCempleados," con ",Cempleados," empleados"
+	Escribir "El empleados con mayor sueldo es DNI ",DNIMaxSue
+	Escribir "Entre todas las sedes hay que abonar $",SueTot," de sueldos", Sue
+
+FinAlgoritmo
+
+Funcion e20y40mil <- Entre25y40mil(sueldo)
+	Definir e20y40mil Como Logico
+	Si sueldo>=25000 Y sueldo<=40000 Entonces
+		e20y40mil <- Verdadero
+	SiNo
+		e20y40mil <- falso
+	FinSi
+FinFuncion
+
+
